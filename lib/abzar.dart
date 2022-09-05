3bad4a38a7b37ddacf0ab54fb07435a515cbbeb0
arash <Arashmohmdi79@gmail.com>
2022-09-05 14:27:09 +0430

    Update abzar.dart

--- lib/abzar.dart
+++ lib/abzar.dart
@@ -22,7 +22,6 @@ export 'package:share_plus/share_plus.dart';
 export 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:video_player/video_player.dart';
-export 'package:webviewx/webviewx.dart';
 
 export 'package:collection/collection.dart';
 export 'components/components.dart';
